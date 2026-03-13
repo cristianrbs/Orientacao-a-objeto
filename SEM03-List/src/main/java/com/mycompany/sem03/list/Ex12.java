2e7db4ece985398055003adc343622899a4bee33
Cristian Rubens <cristian.rubens2006@gmail.com>
2026-03-13 11:09:52 -0300

    Update Ex12.java

--- SEM03-List/src/main/java/com/mycompany/sem03/list/Ex12.java
+++ SEM03-List/src/main/java/com/mycompany/sem03/list/Ex12.java
@@ -3,5 +3,17 @@ import java.util.Scanner;
 import Classes.Musica;
 
 public class Ex12 {
-   
+   public static int imprimeMenu(){
+       Scanner ler = new Scanner(System.in);
+       
+       System.out.print("------------------------------");
+       System.out.print("O que deseja fazer");
+       System.out.print("------------------------------");
+       System.out.print("1 - Adicionar Musica");
+       System.out.print("2 - Remover Musica");
+       System.out.print("3 - Pesquisar Muisica pelo titulo");
+       System.out.print("------------------------------");
+       System.out.println("Escolha uma opcao:");
+       return ler.nextInt();
+   }
 }
