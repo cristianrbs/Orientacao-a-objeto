78912c5cd49ad3ad5b21e6e21105d9ab8985d023
Cristian Rubens <cristian.rubens2006@gmail.com>
2026-03-21 14:07:17 -0300

    creat sem04

---
+++ SEM04-Collections/src/main/java/com/mycompany/sem04/collections/Ex01.java
@@ -0,0 +1,15 @@
+package com.mycompany.sem04.collections;
+import Classes.Fila;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class Ex01 {
+    public static void main(String[] args){
+        Queue<Fila> fila = new LinkedList<>();
+        Fila f1 = new Fila(f1);
+        
+        
+        
+    }
+}
