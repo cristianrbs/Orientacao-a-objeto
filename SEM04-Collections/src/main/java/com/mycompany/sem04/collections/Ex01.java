7f028e4cc4ef7158ca1cfabd8edc33c7215a4f6c
Cristian Rubens <cristian.rubens2006@gmail.com>
2026-03-21 14:37:53 -0300

    update a lot

--- SEM04-Collections/src/main/java/com/mycompany/sem04/collections/Ex01.java
+++ SEM04-Collections/src/main/java/com/mycompany/sem04/collections/Ex01.java
@@ -1,29 +1,29 @@
 package com.mycompany.sem04.collections;
-import Classes.Fila;
 import java.util.LinkedList;
 import java.util.Queue;
 
-
 public class Ex01 {
     public static void main(String[] args){
-        Queue<Fila> fila = new LinkedList<>();
-        Fila c1 = new Fila("Cristian", "744.871.308-67", 19);
-        Fila c2 = new Fila("Rubens", "325.654.159-31", 20);
-        Fila c3 = new Fila("Oliveira", "048.066.454-43", 21);
-        Fila c4 = new Fila("Carmo", "7955.631.815-10", 22);
-        Fila c5 = new Fila("Osvaldo", "347.659.387-86", 23);
+        Queue<String> fila = new LinkedList<>();
         
-        fila.add(c1);
-        fila.add(c2);
-        fila.add(c3);
-        fila.add(c4);
-        fila.add(c5);
+        fila.add("Cristian");
+        fila.add("Rubens");
+        fila.add("Oliveira");
+        fila.add("Carmo");
+        fila.add("Bastião");
         
         while (!fila.isEmpty()) {
-        Fila pessoa = fila.poll();
-        Fila proximo = fila.peek();
+        String pessoa = fila.poll();
+        String aguardando = fila.peek();
+        
         System.out.println("Atendendo: " + pessoa);
-        System.out.println("Próximo: " + proximo);
-}
+        
+        if (aguardando != null) {
+            System.out.println("Aguardando: " + aguardando);
+        } 
+        else {
+            System.out.println("Fila vazia: " + fila.isEmpty());
+         }
+       }
     }
 }
