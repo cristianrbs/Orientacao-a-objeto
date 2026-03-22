9f0b918d2e8f4c6ab715e48a274615cf6135929b
Cristian Rubens <cristian.rubens2006@gmail.com>
2026-03-22 15:02:22 -0300

    create ex07

---
+++ SEM04-Collections/src/main/java/com/mycompany/sem04/collections/Ex07.java
@@ -0,0 +1,17 @@
+package com.mycompany.sem04.collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Ex07 {
+    public static void main(String[] args){
+        Map<Character, Integer> palavra = new HashMap<>();
+        String texto = "hello world";
+        
+        for(char t: texto.toCharArray()){
+            Integer cont = palavra.getOrDefault(t, 0);
+            palavra.put(t, cont + 1);
+        }
+        System.out.println(palavra);
+    }
+}
