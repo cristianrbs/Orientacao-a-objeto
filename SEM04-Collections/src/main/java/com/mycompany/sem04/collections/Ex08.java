6d8c4b22691bc1676bacf442345d7d95421aba09
Cristian Rubens <cristian.rubens2006@gmail.com>
2026-03-22 15:13:11 -0300

    create and update ex08

--- SEM04-Collections/src/main/java/com/mycompany/sem04/collections/Ex08.java
+++ SEM04-Collections/src/main/java/com/mycompany/sem04/collections/Ex08.java
@@ -1,9 +1,19 @@
 package com.mycompany.sem04.collections;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- *
- * @author cristian
- */
 public class Ex08 {
-    
+    public static void main(String[] args){
+        HashMap<Integer, String> map = new HashMap<>();
+        HashMap<String, Integer> invercao = new HashMap<>();
+        map.put(1, "a");
+        map.put(2, "b");
+        map.put(3, "c");
+        
+        
+        for(Map.Entry<Integer,String> m: map.entrySet()){
+            invercao.put(m.getValue(), m.getKey());
+        }
+        System.out.println(invercao);
+    }
 }
