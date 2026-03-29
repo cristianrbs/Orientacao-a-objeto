fa0a85ca40e308b4f0fd6de482bc8f16475f380a
Cristian Rubens <cristian.rubens2006@gmail.com>
2026-03-29 10:42:05 -0300

    Update GerenciadorDeReserva.java

--- SEM04-Gerenciador-Lista/src/main/java/Classes/GerenciadorDeReserva.java
+++ SEM04-Gerenciador-Lista/src/main/java/Classes/GerenciadorDeReserva.java
@@ -13,12 +13,14 @@ public class GerenciadorDeReserva {
     
     public void adicionarReserva(Reserva reserva){
         reservas.add(reserva);
+        System.out.println("Reserva adicionada");
     }
     
     public boolean calcelarReserva(String nomeReserva){
         for(Reserva r: reservas){
             if(r.getNomeReserva().equals(nomeReserva)){
                 reservas.remove(r);
+                System.out.println("Reserva cancelada");
                 return true;
             }
         }
@@ -34,6 +36,7 @@ public class GerenciadorDeReserva {
     public Reserva buscarReserva(String nomeReserva){
         for(Reserva r: reservas){
             if(r.getNomeReserva().equalsIgnoreCase(nomeReserva)){
+                System.out.println("Reserva encontrada");
                 return r;
             }
         }
