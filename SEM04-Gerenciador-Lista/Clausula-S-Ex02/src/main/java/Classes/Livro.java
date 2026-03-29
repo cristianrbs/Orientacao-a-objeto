ddae56024f8fa462bff5072cf0d82e83b7b6b094
Cristian Rubens <cristian.rubens2006@gmail.com>
2026-03-29 19:21:25 -0300

    create class livro

---
+++ SEM04-Gerenciador-Lista/Clausula-S-Ex02/src/main/java/Classes/Livro.java
@@ -0,0 +1,32 @@
+
+package Classes;
+
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int paginas;
+    public static String NomeDaBiblioteca;
+
+    public Livro(String titulo, String autor, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+        NomeDaBiblioteca = "Havard";
+    }
+    
+    public Livro(){
+        this.titulo = "";
+        this.autor = "";
+        this.paginas = 0;
+        NomeDaBiblioteca = "Havard";
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" + "titulo=" + titulo + ", autor=" + autor + ", paginas=" + paginas + '}';
+    }
+    
+    
+    
+}
